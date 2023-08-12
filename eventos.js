cb0bcd792e5c275debd9fa87b1d7e192818f3037
rodrigotabeira <tabeirarodrigo8@gmail.com>
2023-08-11 21:11:37 -0300

    Solución Click
    Se agregó en el JS una constante para seleccionar dentro del div contenedor el button. Luego a través de esa constante se creo el evento stopPropagation para que no salga la alerta del div.

--- eventos.js
+++ eventos.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const divContenedor = document.querySelector('.contenedor');
+    const botonClick = divContenedor.querySelector('button');
     
     divContenedor.addEventListener('click', function() {
         alert('Hola! Soy el div');
+    });
+
+    botonClick.addEventListener('click', function(event) {
+        event.stopPropagation();
     });
 });
